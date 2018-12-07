bd18631ea92aad049b5e9672da56c7791b7e72b0
sl4d1c <slawa.minden@freenet.de>
2018-12-07 11:37:42 +0100

    saving progress

--- Model/InventorySourceRepository.php
+++ Model/InventorySourceRepository.php
@@ -5,14 +5,20 @@ namespace Mash2\Cobby\Model;
 
 class InventorySourceRepository implements \Mash2\Cobby\Api\InventorySourceRepositoryInterface
 {
-    public function __construct(){}
+    private $sources;
+
+    public function __construct(
+        \Magento\InventoryImportExport\Model\Export\Sources $sources
+    ){
+        $this->sources = $sources;
+    }
 
 
     public function export()
     {
         $result = array();
 
-
+        $sources = $this->sources->export();
 
 
         return $result;
