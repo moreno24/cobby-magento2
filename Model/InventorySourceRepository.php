7c5a0a8eace83f9f83b0a99ea9e82a2fe762b6e7
sl4d1c <slawa.minden@freenet.de>
2018-12-07 10:03:46 +0100

    added class and route for export of inventory sources

---
+++ Model/InventorySourceRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mash2\Cobby\Model;
+
+
+class InventorySourceRepository implements \Mash2\Cobby\Api\InventorySourceRepositoryInterface
+{
+    public function __construct(){}
+
+
+    public function export()
+    {
+        $result = array();
+
+
+
+
+        return $result;
+    }
+}
